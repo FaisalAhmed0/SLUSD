6b313dec24f5b59c020e77126d71fdc7450c23fe
FaisalAhmed0 <faisal.ahmed.a.m@gmail.com>
2021-11-23 10:06:17 +0000

    add regularizations to the disc.

--- src/callbacks/callbacks.py
+++ src/callbacks/callbacks.py
@@ -43,8 +43,10 @@ class VideoRecorderCallback(BaseCallback):
 
     def _on_step(self) -> bool:
         if self.n_calls % self._record_freq == 0:
-          record_video(self._eval_env_id, self.model, n_z=self.n_z, video_folder=self.videos_dir, n_calls=self.n_calls)            
+            record_video(self._eval_env_id, self.model, n_z=self.n_z,
+                         video_folder=self.videos_dir, n_calls=self.n_calls)
         return True
+
 
 class DiscriminatorCallback(BaseCallback):
     """
@@ -52,21 +54,48 @@ class DiscriminatorCallback(BaseCallback):
 
     :param verbose: (int) Verbosity level 0: not output 1: info 2: debug
     """
-    def __init__(self, discriminator, buffer, hyerparams , sw, n_skills, min_buffer_size=2048, verbose=0, save_dir="./", on_policy=False):
+    # TODO: add smoothing parameter
+    # TODO: add gradient penalties gp
+    # TODO: add mixup regularization
+    def __init__(self, discriminator, buffer, hyerparams, sw, n_skills, min_buffer_size=2048, verbose=0, save_dir="./",
+                 on_policy=False, weight_decay=None, label_smoothing=None, gp=None, mixup=False):
         super(DiscriminatorCallback, self).__init__(verbose)
+        # classifier
         self.d = discriminator
+        # data buffer
         self.buffer = buffer
+        # optimization hyperparameters
         self.hyerparams = hyerparams
-        self.optimizer = opt.Adam(self.d.parameters(), lr=hyerparams['learning_rate'])
+        # TODO: Added Weight decay
+        self.weight_decay = weight_decay if weight_decay else 0
+        # optimizer
+        self.optimizer = opt.Adam(
+            self.d.parameters(), lr=hyerparams['learning_rate'], weight_decay=weight_decay)
+        # number of epochs
         self.epochs = hyerparams['n_epochs']
+        # batch size
         self.batch_size = hyerparams['batch_size']
-        self.criterion = F.cross_entropy
+        # use label smoothing if not None, otherwise use cross_entropy
+        if label_smoothing:
+            self.criterion = self.label_smoothedCrossEntropyLoss
+        else:
+            self.criterion = F.cross_entropy
+        # tensorboard summary writer
         self.sw = sw
+        # number of skills
         self.n_skills = n_skills
+        # minimum size of the buffer before training
         self.min_buffer_size = min_buffer_size
+        # directory to save the model parameters
         self.save_dir = save_dir
+        # boolean for the algorithm type
         self.on_policy = on_policy
-    def _on_step(self) -> bool:
+        # gradient penalty
+        self.gp = gp
+        # mixup regularization
+        self.mixup = mixup
+
+    def _on_step(self):
         """
         This method will be called by the model after each call to `env.step()`.
 
@@ -76,24 +105,24 @@ class DiscriminatorCallback(BaseCallback):
         :return: (bool) If the callback returns False, training is aborted early.
         """
         if self.on_policy:
-          for obs_vec in self.locals['obs_tensor']:
-            env_obs, skill = self.split_obs(obs_vec)
-            obs = torch.clone(env_obs)
-            skill = torch.tensor(skill)
-            self.buffer.add(obs, skill)
+            for obs_vec in self.locals['obs_tensor']:
+                env_obs, skill = self.split_obs(obs_vec)
+                obs = torch.clone(env_obs)
+                skill = torch.tensor(skill)
+                self.buffer.add(obs, skill)
 
         return True
 
     def split_obs(self, obs):
         if self.on_policy:
-          obs = obs.cpu()
-          env_obs = obs[: -self.n_skills]
-          skill, = np.where( obs[-self.n_skills: ] == 1 )
-          return env_obs, skill[0]
+            obs = obs.cpu()
+            env_obs = obs[: -self.n_skills]
+            skill, = np.where(obs[-self.n_skills:] == 1)
+            return env_obs, skill[0]
         else:
-          env_obs = torch.clone(obs[:, : -self.n_skills])
-          skills  =  torch.argmax(obs[:, -self.n_skills: ], dim=1)
-          return env_obs, skills
+            env_obs = torch.clone(obs[:, : -self.n_skills])
+            skills = torch.argmax(obs[:, -self.n_skills:], dim=1)
+            return env_obs, skills
 
     def gradient_norm(self, net):
         '''
@@ -104,53 +133,124 @@ class DiscriminatorCallback(BaseCallback):
         total_norm = 0
         total_weights_norm = 0
         for param in net.parameters():
-          param_norm = param.grad.detach().data.norm(2)
-          total_weights_norm += param.detach().data.norm(2) ** 2
-          total_norm += param_norm.item() ** 2
+            param_norm = param.grad.detach().data.norm(2)
+            total_weights_norm += param.detach().data.norm(2) ** 2
+            total_norm += param_norm.item() ** 2
         return total_norm**0.5, total_weights_norm**0.5
-          
 
-    def _on_rollout_end(self) -> None:
+    # TODO: Add a label smoothing function
+    # most of the function is from https://github.com/pytorch/pytorch/issues/7455#issuecomment-513062631
+    def label_smoothedCrossEntropyLoss(self, preds, targets, smoothing=0.2):
+        num_classes = preds.shape[1]
+        print(
+            f"Number of classes in the label_smoothedCrossEntropyLoss: {num_classes}")
+        print(f"targets: {targets}")
+        with torch.no_grad():
+            smooth_target = torch.zeros_like(preds)
+            smooth_target.fill_(smoothing/(num_classes-1))
+            smooth_target.scatter_(1, targets.unsqueeze(1), 1 - smoothing)
+        return torch.mean(torch.sum(- smooth_target * torch.log_softmax(preds, dim=-1), dim=-1))
+
+    # TODO: added onehot cross entropy
+    def onehot_cross_entropy(self, preds, targets):
+        preds_logs = torch.log_softmax(preds, dim=-1)
+        with torch.no_grad():
+            onehots = torch.zeros(self.batch_size, preds.shape[1])
+            onehots[torch.arange(self.batch_size), targets] = 1
+        return torch.mean(torch.sum(- onehots * preds_logs, dim=1))
+
+
+    # TODO: added gradient penalty function
+    def grad_penalty(self, loss, inputs):
+        '''
+        A function to compute the gradient penalty
+        loss: the loss value
+        input: the inputs used in the loss computation
+        '''
+        grads = torch.autograd.grad(loss, inputs, retain_graph=True)
+        # print(grad)
+        grad_norm = 0
+        for grad in grads:
+            grad_norm += torch.norm(grad)
+        gradient_penalty = (grad_norm - 1).pow(2)
+        return gradient_penalty
+
+    # TODO: Add mixup r
+    @torch.no_grad()
+    def mixup_reg(self, x1, x2, targets=False):
+        # sample the mixing factor
+        m = np.random.beta(a=0.4, b=0.4)
+        batch_size = self.batch_size
+        if targets:
+            y1 = torch.zeros_like(x2)
+            y1[torch.arange(batch_size), x1] = 1
+            y2 = torch.zeros_like(x2)
+            y2[torch.arange(batch_size), x2] = 1
+            return m*y1 + (1-m) * y2
+        return m*x1 + (1-m) * x2
+
+    def _on_rollout_end(self):
         """
         This event is triggered before updating the policy.
         """
-        current_buffer_size = len(self.buffer) if self.on_policy else self.locals['replay_buffer'].buffer_size if self.locals['replay_buffer'].full else self.locals['replay_buffer'].pos
+        current_buffer_size = len(
+            self.buffer) if self.on_policy else self.locals['replay_buffer'].buffer_size if self.locals['replay_buffer'].full else self.locals['replay_buffer'].pos
         if current_buffer_size >= self.min_buffer_size:
             # initlize the average losses
             epoch_loss = 0
             for _ in range(1):
-              if self.on_policy:
-                inputs, targets = self.buffer.sample(self.batch_size)
-              else:
-                obs, _, _, _, _= self.locals['replay_buffer'].sample(self.batch_size)
-                inputs, targets = self.split_obs(obs)
-              outputs = self.d(inputs.to(conf.device))
-              loss = self.criterion(outputs, targets.to(conf.device).to(torch.int64))
-              self.optimizer.zero_grad()
-              loss.backward()
-              # clip_grad_norm_(model.parameters(), 0.1)
-              self.optimizer.step()
-              epoch_loss += loss.cpu().detach().item()
-            
+                if self.on_policy:
+                    inputs, targets = self.buffer.sample(self.batch_size)
+                else:
+                    obs, _, _, _, _ = self.locals['replay_buffer'].sample(
+                        self.batch_size)
+                    inputs, targets = self.split_obs(obs)
+                outputs = self.d(inputs.to(conf.device))
+                loss = self.criterion(outputs, targets.to(
+                    conf.device).to(torch.int64))
+                # TODO: add mixup
+                if self.mixup:
+                    inputs2, targets2 = self.buffer.sample(self.batch_size)
+                    inputs = self.mixup_reg(input, inputs2)
+                    targets = self.mixup_reg(targets, targets2, targets=True)
+                    loss = 0
+                    loss = self.onehot_cross_entropy(outputs, targets)
+                # TODO: Add gradient penalty to the loss
+                if self.gp:
+                    inputs.requires_grad_(True)
+                    gp = self.grad_penalty(loss, inputs)
+                    loss += (self.gp * gp)
+                self.optimizer.zero_grad()
+                loss.backward()
+                # clip_grad_norm_(model.parameters(), 0.1)
+                self.optimizer.step()
+                epoch_loss += loss.cpu().detach().item()
+
             grad_norms, weights_norm = self.gradient_norm(self.d)
             epoch_loss /= 1
-            if (not self.on_policy) and self.num_timesteps%100 == 0:
-                self.sw.add_scalar("discrimiator loss", epoch_loss, self.num_timesteps)
-                self.sw.add_scalar("discrimiator grad norm", grad_norms, self.num_timesteps)
-                self.sw.add_scalar("discrimiator wights norm", weights_norm, self.num_timesteps)
+            if (not self.on_policy) and self.num_timesteps % 100 == 0:
+                self.sw.add_scalar("discrimiator loss",
+                                   epoch_loss, self.num_timesteps)
+                self.sw.add_scalar("discrimiator grad norm",
+                                   grad_norms, self.num_timesteps)
+                self.sw.add_scalar("discrimiator wights norm",
+                                   weights_norm, self.num_timesteps)
                 torch.save(self.d.state_dict(), self.save_dir + "/disc.pth")
             else:
-                self.sw.add_scalar("discrimiator loss", epoch_loss, self.num_timesteps)
-                self.sw.add_scalar("discrimiator grad norm", grad_norms, self.num_timesteps)
-                self.sw.add_scalar("discrimiator wights norm", weights_norm, self.num_timesteps)
+                self.sw.add_scalar("discrimiator loss",
+                                   epoch_loss, self.num_timesteps)
+                self.sw.add_scalar("discrimiator grad norm",
+                                   grad_norms, self.num_timesteps)
+                self.sw.add_scalar("discrimiator wights norm",
+                                   weights_norm, self.num_timesteps)
                 torch.save(self.d.state_dict(), self.save_dir + "/disc.pth")
-                
 
 
 class FineTuneCallback(BaseCallback):
     '''
     A callback to check the peroformance of the downstream task every N steps 
     '''
+
     def __init__(self, args, params, alg_params, conf, seed, alg, timestamp, n_samples=4):
         super().__init__()
         self.args = args
@@ -165,8 +265,7 @@ class FineTuneCallback(BaseCallback):
         # input()
         self.timestamp = timestamp
         self.alg = alg
-        
-        
+
     def _on_step(self):
         """
         This method will be called by the model after each call to `env.step()`.
@@ -186,11 +285,15 @@ class FineTuneCallback(BaseCallback):
 
         return True
 
-    
     # finetune the pretrained policy on a specific task
+
     def finetune(self, steps):
-        model_dir = self.conf.pretrain_steps_exper_dir + f"{self.args.env}/" + f"{self.alg}_{self.args.env}_skills:{self.params['n_skills']}_{self.timestamp}"
-        self.directory = self.conf.pretrain_steps_exper_dir + f"{self.args.env}/" + f"{self.alg}_{self.args.env}_skills:{self.params['n_skills']}_pretrain_steps:{steps}_{self.timestamp}"
+        model_dir = self.conf.pretrain_steps_exper_dir + \
+            f"{self.args.env}/" + \
+            f"{self.alg}_{self.args.env}_skills:{self.params['n_skills']}_{self.timestamp}"
+        self.directory = self.conf.pretrain_steps_exper_dir + \
+            f"{self.args.env}/" + \
+            f"{self.alg}_{self.args.env}_skills:{self.params['n_skills']}_pretrain_steps:{steps}_{self.timestamp}"
         os.makedirs(self.directory)
         env = DummyVecEnv([lambda: SkillWrapper(gym.make(
             self.env_name), self.params['n_skills'], max_steps=self.conf.max_steps)])
@@ -208,7 +311,7 @@ class FineTuneCallback(BaseCallback):
 
         if self.alg == "sac":
             env = DummyVecEnv([lambda: SkillWrapperFinetune(Monitor(gym.make(
-        self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index)])
+                self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index)])
             model = SAC('MlpPolicy', env, verbose=1,
                         learning_rate=self.alg_params['learning_rate'],
                         batch_size=self.alg_params['batch_size'],
@@ -223,12 +326,12 @@ class FineTuneCallback(BaseCallback):
                         )
         elif self.alg == "ppo":
             env = DummyVecEnv([lambda: SkillWrapperFinetune(Monitor(gym.make(
-        self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index), 
-                              lambda: SkillWrapperFinetune(Monitor(gym.make(
-        self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index), lambda: SkillWrapperFinetune(Monitor(gym.make(
-        self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index), lambda: SkillWrapperFinetune(Monitor(gym.make(
-        self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index)])
-            
+                self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index),
+                lambda: SkillWrapperFinetune(Monitor(gym.make(
+                    self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index), lambda: SkillWrapperFinetune(Monitor(gym.make(
+                        self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index), lambda: SkillWrapperFinetune(Monitor(gym.make(
+                            self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index)])
+
             model = PPO('MlpPolicy', env, verbose=1,
                         learning_rate=self.alg_params['learning_rate'],
                         n_steps=self.alg_params['n_steps'],
@@ -242,24 +345,25 @@ class FineTuneCallback(BaseCallback):
                         tensorboard_log=self.directory,
 
                         )
-        
+
         # env = DummyVecEnv([lambda: SkillWrapperFinetune(Monitor(gym.make(
         # self.env_name),  f"{self.directory}/finetune_train_results"), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index)])
 
         eval_env = SkillWrapperFinetune(gym.make(
             self.env_name), self.params['n_skills'], max_steps=gym.make(self.env_name)._max_episode_steps, skill=best_skill_index)
         eval_env = Monitor(eval_env, f"{self.directory}/finetune_eval_results")
-        
+
         eval_callback = EvalCallback(eval_env, best_model_save_path=self.directory + f"/best_finetuned_model_skillIndex:{best_skill_index}",
-                                    log_path=f"{self.directory}/finetune_eval_results", eval_freq=1000,
-                                    deterministic=True, render=False)
+                                     log_path=f"{self.directory}/finetune_eval_results", eval_freq=1000,
+                                     deterministic=True, render=False)
         if self.alg == "sac":
-            model = SAC.load(model_dir, env=env, tensorboard_log=self.directory)
+            model = SAC.load(model_dir, env=env,
+                             tensorboard_log=self.directory)
             model.learn(total_timesteps=self.params['finetune_steps'],
                         callback=eval_callback, tb_log_name="SAC_FineTune")
         elif self.alg == "ppo":
             model = PPO.load(model_dir, env=env, tensorboard_log=self.directory,
-                            clip_range=get_schedule_fn(self.alg_params['clip_range']))
+                             clip_range=get_schedule_fn(self.alg_params['clip_range']))
 
             model.learn(total_timesteps=self.params['finetune_steps'],
                         callback=eval_callback, tb_log_name="PPO_FineTune")
@@ -267,5 +371,3 @@ class FineTuneCallback(BaseCallback):
         # record a video of the agent
         # record_video_finetune(self.env_name, best_skill_index, model,
         #                     self.params['n_skills'], video_length=1000, video_folder='videos_finetune/', alg=self.alg)
-
-
--- src/models/models.py
+++ src/models/models.py
@@ -3,10 +3,12 @@ import torch.nn as nn
 class Discriminator(nn.Module):
   '''
   Simple MLP classifier
-  n_input: size of the inputs (observation dimension)
-  n_hiddens: list of hidden layers sizes
+  n_input: size of the inputs (observation dimension).
+  n_hiddens: list of hidden layers sizes.
+  n_skills: Number of skills.
+  dropout: dropout probability.
   '''
-  def __init__(self, n_input, n_hiddens, n_skills):
+  def __init__(self, n_input, n_hiddens, n_skills, dropout=None):
     super().__init__()
     layers = []
     layers.append(nn.Linear(n_input, n_hiddens[0]))
@@ -14,9 +16,14 @@ class Discriminator(nn.Module):
     for i in range(len(n_hiddens)-1):
       layers.append( nn.Linear(n_hiddens[i], n_hiddens[i+1]) )
       layers.append( nn.ReLU() )
+      # TODO: Added dropout
+      if dropout:
+        layers.append( nn.Dropout(dropout) )
     
     self.head = nn.Sequential(*layers)
     self.output = nn.Linear(n_hiddens[-1], n_skills)
+    # TODO: print head to check
+    print(f"Discriminator head {self.head}")
   
 
   def forward(self, x):
