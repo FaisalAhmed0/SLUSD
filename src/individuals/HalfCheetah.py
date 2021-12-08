b55d0f06e40c7c318da13f1d2d91891191f5014c
root <root@deeplearning-2-vm.c.primeval-hearth-328720.internal>
2021-12-08 17:57:03 +0000

    fixedHalfCheetah indiv.

--- src/individuals/HalfCheetah.py
+++ src/individuals/HalfCheetah.py
@@ -3,6 +3,7 @@ This file define an indiviual for Evolution startigies algorithms
 '''
 import gym 
 import numpy as np
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,74 +13,58 @@ from torch.distributions import MultivariateNormal
 
 import tqdm as tqdm
 
-from evostrat import compute_centered_ranks, NormalPopulation
-
-
 from typing import Dict
 
 from evostrat import Individual
 
-from src.models import MLP_policy
-from src.congif import conf
-from src.env_wrappers import SkillWrapper, RewardWrapper, SkillWrapperFinetune
+from src.models.models import MLP_policy
+from src.config import conf
+from src.environment_wrappers.env_wrappers import SkillWrapper, RewardWrapper, SkillWrapperFinetune
 
 
 class HalfCheetah(Individual):
     """
-    Swimmer controlled by a feedforward policy network
+    HalfCheetah controlled by a feedforward policy network
     """
-
+    # Static variables
+    d = None # the discriminator
+    n_skills = None # the number of skills
+    skill = None # skill for the case of finetuning
     def __init__(self):
         self.net = MLP_policy(17 + conf.n_Z, [conf.layer_size_policy, conf.layer_size_policy], 6)
-        self.d = None
-        self.n_skills = None
-        self.buffer = None
-        self.skill = None
+        self.conf = conf
+        self.t = 0
 
     @staticmethod
     def from_params(params: Dict[str, t.Tensor]) -> 'HalfCheetah':
         agent = HalfCheetah()
         agent.net.load_state_dict(params)
         return agent
-    
-    def set_discriminator(self, d):
-        self.d = d
-        
-    def set_n_skills(self, n_skills):
-        self.n_skills = n_skills
-        
-    def set_dataBuffer(self, buffer):
-        self.buffer = buffer
-        
-    def set_skill(self, skill):
-        self.skill = skill
-
 
     def fitness(self, render=False) -> float:
-        print(f"Discriminator is {self.d}")
-        assert not (self.d == None)
-        assert not (self.n_skills == None)
-        assert not (self.buffer == None)
-        if self.skill:
-            env = SkillWrapperFinetune(gym.make("HalfCheetah-v2"), self.n_skills, max_steps=self.conf.max_steps, skill=self.skill)
+        assert not (HalfCheetah.d == None)
+        assert not (HalfCheetah.n_skills == None)
+        # save the data for the discriminator replay buffer
+        data = []
+        if HalfCheetah.skill:
+            env = SkillWrapperFinetune(gym.make("HalfCheetah-v2"), HalfCheetah.n_skills, max_steps=self.conf.max_steps, skill=HalfCheetah.skill)
         else:
-            env = RewardWrapper(SkillWrapper(gym.make("HalfCheetah-v2"), self.n_skills max_steps=self.conf.max_steps), self.d, self.n_skills)
+            env = RewardWrapper(SkillWrapper(gym.make("HalfCheetah-v2"), HalfCheetah.n_skills, max_steps=self.conf.max_steps), HalfCheetah.d, HalfCheetah.n_skills)
         obs = env.reset()
         done = False
         r_tot = 0
+        self.t += 1
         while not done:
+            self.t += 1
             env_obs, skill = self.split_obs(obs)
             env_obs = torch.clone(env_obs)
-            skill = torch.tensor(skill)
-            self.buffer.add(env_obs, skill)
+            skill = torch.tensor(skill.numpy())
+            data.append((env_obs.numpy(), skill.item()))
             action = self.action(obs)
             obs, r, done, _ = env.step(action)
             r_tot += r
-            if render:
-                env.render()
-
         env.close()
-        return r_tot
+        return r_tot, data, self.t
 
     def get_params(self) -> Dict[str, t.Tensor]:
         return self.net.state_dict()
@@ -93,7 +78,7 @@ class HalfCheetah(Individual):
             return self.net.sample_action(torch.tensor(obs).unsqueeze(dim=0)).numpy()
         
     def split_obs(self, obs):
-        env_obs = torch.clone(obs[:, : -self.n_skills])
-        skills = torch.argmax(obs[:, -self.n_skills:], dim=1)
+        env_obs = torch.clone(torch.tensor(obs[: -HalfCheetah.n_skills]))
+        skills = torch.argmax(torch.tensor(obs[-HalfCheetah.n_skills:]), dim=-1)
         return env_obs, skills
 
