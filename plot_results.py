980f98c30b2a356374ac9c3056a8b75c9c164855
root <root@deeplearning-2-vm.c.primeval-hearth-328720.internal>
2021-12-16 21:12:13 +0000

    fixed plot results

--- plot_results.py
+++ plot_results.py
@@ -1,4 +1,6 @@
 import argparse
+import ast
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,9 +15,12 @@ import gym
 def cmd_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, default="MountainCarContinuous-v0")
-    parser.add_argument("--alg", type=str, default="ppo")
+    parser.add_argument("--algs", nargs="*", type=str, default="ppo") # pass the algorithms that correspond to each timestamp
     parser.add_argument("--skills", type=int, default=4)
-    parser.add_argument("--stamp", type=str, required=True)
+    parser.add_argument("--stamps", nargs="*", type=str, required=True) # pass the timestamps as a list
+    parser.add_argument("--colors", nargs="*", type=str, required=False, default=['b', 'r']) # pass the timestamps as a list
+    
+    # parser.add_argument("--stamp", type=str, required=True)
     args = parser.parse_args()
     return args
 
@@ -49,31 +54,69 @@ def plot(x, y, xlabel, ylabel, legend, color, filename):
 
 if __name__ == "__main__":
     args = cmd_args()
-    # file_dir = conf.log_dir_finetune + f"{args.alg}_{args.env}_{args.stamp}/" + "eval_results/" + "evaluations.npz"
-    # file_dir = conf.log_dir_finetune + f"{args.alg}_{args.env}_{args.stamp}/" + "evaluations.npz"
-    print("Skills evaluation")
-    file_dir_skills = conf.log_dir_finetune +  f"{args.alg}_{args.env}_skills:{args.skills}_{args.stamp}/" + "eval_results/" + "evaluations.npz"
-    files = np.load(file_dir_skills)
-    steps = files['timesteps']
-    results = files['results']
-    plot(steps, results, "Timesteps", "Reward", f"{args.alg}", 'b', f"{args.alg}_{args.env}_skills")
-    
-    print("Finetine evaluation")
-    file_dir_finetune = conf.log_dir_finetune +  f"{args.alg}_{args.env}_skills:{args.skills}_{args.stamp}/" + "finetune_eval_results/" + "evaluations.npz"
-    files = np.load(file_dir_finetune)
-    steps = files['timesteps']
-    results = files['results']
-    plot(steps, results, "Timesteps", "Reward", f"{args.alg}", 'b', f"{args.alg}_{args.env}_finetune")
-    
-    print("Plot for all algorithms")
-    algs = ["ppo", "sac"]
-    stamps = ["1637837247.0089962", "1637838633.2888947"]
-    colors = ['r', 'b']
-    plt.figure()
+    algs = args.algs
+    stamps = args.stamps
+    colors = args.colors
     xlabel = "Timesteps"
     ylabel = "Reward"
+    # file_dir = conf.log_dir_finetune + f"{args.alg}_{args.env}_{args.stamp}/" + "eval_results/" + "evaluations.npz"
+    # file_dir = conf.log_dir_finetune + f"{args.alg}_{args.env}_{args.stamp}/" + "evaluations.npz"
+    plt.figure()
+    plt.title(f"{args.env[: args.env.index('-')]}")
+    print("######## Skills evaluation ########")
+    for i in range(len(algs)):
+        alg = algs[i]
+        print(f"Algorithm: {alg}")
+        plot_color = colors[i]
+        stamp = stamps[i]
+        legend = alg
+        file_dir_skills = conf.log_dir_finetune +  f"{alg}_{args.env}_skills:{args.skills}_{stamp}/" + "eval_results/" + "evaluations.npz"
+        files = np.load(file_dir_skills)
+        steps = files['timesteps']
+        results = files['results']
+        data_mean = results.mean(axis=1)
+        # print(len(data_mean))
+        data_mean = np.convolve(data_mean, np.ones(10)/10, mode='valid') 
+        data_std = results.std(axis=1)
+        data_std = np.convolve(data_std, np.ones(10)/10, mode='valid') 
+        print(f"mean: {data_mean[-1]}")
+        print(f"std: {data_std[-1]}")
+        steps = np.convolve(steps, np.ones(10)/10, mode='valid') 
+        if len(algs) > 1:
+            plt.plot(steps, data_mean, label=legend, color=plot_color)
+            plt.legend()
+        else:
+            plt.plot(steps, data_mean, color=plot_color)
+        plt.fill_between(steps, (data_mean-data_std), (data_mean+data_std), color=plot_color, alpha=0.3)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.grid(False)
+        if len(algs) == 1:
+            filename = f"{args.env}_pretraining_{alg}"
+            files_dir = f"Vis/{args.env}"
+            os.makedirs(files_dir, exist_ok=True)
+            plt.savefig(f'{files_dir}/{filename}', dpi=150)    
+    if len(algs) > 1:
+        filename = f"{args.env}_pretraining_all_algs"
+        files_dir = f"Vis/{args.env}"
+        os.makedirs(files_dir, exist_ok=True)
+        plt.savefig(f'{files_dir}/{filename}', dpi=150)    
+    
+    # print("Finetine evaluation")
+    # file_dir_finetune = conf.log_dir_finetune +  f"{args.alg}_{args.env}_skills:{args.skills}_{args.stamp}/" + "finetune_eval_results/" + "evaluations.npz"
+    # files = np.load(file_dir_finetune)
+    # steps = files['timesteps']
+    # results = files['results']
+    # plot(steps, results, "Timesteps", "Reward", f"{args.alg}", 'b', f"{args.alg}_{args.env}_finetune")
+    
+    # print("Plot for all algorithms")
+    
     # x = steps
     # y = results
+    # loop throgh the finetuning results
+    plt.figure()
+    plt.title(f"{args.env[: args.env.index('-')]}")
+    print("######## Finetine evaluation ########")
     for i in range(len(algs)):
         alg = algs[i]
         print(f"Algorithm: {alg}")
@@ -97,16 +140,25 @@ if __name__ == "__main__":
         data_std = data_std / gym.make(args.env).spec.reward_threshold
         # normalize
         # print(len(x))
-        plt.plot(steps, data_mean, label=legend, color=plot_color)
+        if len(algs) > 1:
+            plt.plot(steps, data_mean, label=legend, color=plot_color)
+            plt.legend()
+        else:
+            plt.plot(steps, data_mean, color=plot_color)
         plt.fill_between(steps, (data_mean-data_std), (data_mean+data_std), color=plot_color, alpha=0.3)
         plt.xlabel(xlabel)
         plt.ylabel(ylabel)
-        plt.grid(True)
-        plt.legend()
-    filename = f"{args.env}_finetune_all_algs"
-    files_dir = f"Vis/{args.env}"
-    os.makedirs(files_dir, exist_ok=True)
-    plt.savefig(f'{files_dir}/{filename}', dpi=150)    
+        plt.grid(False)
+        if len(algs) == 1:
+            filename = f"{args.env}_finetune_{alg}"
+            files_dir = f"Vis/{args.env}"
+            os.makedirs(files_dir, exist_ok=True)
+            plt.savefig(f'{files_dir}/{filename}', dpi=150)    
+    if len(algs) > 1:
+        filename = f"{args.env}_finetune_all_algs"
+        files_dir = f"Vis/{args.env}"
+        os.makedirs(files_dir, exist_ok=True)
+        plt.savefig(f'{files_dir}/{filename}', dpi=150)    
         
         
         
