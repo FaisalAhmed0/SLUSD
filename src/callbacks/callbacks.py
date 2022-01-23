3be871823f044319f4978b961f9d2d0eb61cc61a
root <root@deeplearning--scalability-vm.c.primeval-hearth-328720.internal>
2022-01-23 21:11:30 +0000

    Fixed Y-axis label and hyperparameters

--- src/callbacks/callbacks.py
+++ src/callbacks/callbacks.py
@@ -350,7 +350,7 @@ class EvaluationCallback(BaseCallback):
             plt.figure()
             plt.plot(self.intr_rewards, self.extr_rewards, label=self.alg.upper())
             plt.xlabel("Intrinsic Reward")
-            plt.ylabel("Extrinsic Reward (Best Skill))
+            plt.ylabel("Extrinsic Reward (Best Skill)")
             plt.legend()
             plt.tight_layout()
             filename = f"Scalability_Experiment_realtime_env:{self.env_name}_alg:{self.alg}_xaxis:Intrinsic Reward.png"
